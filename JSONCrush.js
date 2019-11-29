0b2868de2484869c526d9d21f8b1c298b61cf0b6
Frank Force <frankforce@gmail.com>
2019-11-29 00:06:50 -0600

    improve speed for long strings and other fixes
    - cap max length of substring to 50 which helps greatly with speed
    - check if every unescaped character is used and switch to bigger char set
    - fix last character ~ not being used
    - code cleanup

--- JSONCrush.js
+++ JSONCrush.js
@@ -7,67 +7,103 @@
 
 function JSONCrush(string)
 {
+    const JSCrush=(string, characters)=>
+    {
+        // JSCrush Algorithm (remove repeated substrings)
+        const ByteLength=string=>encodeURI(string).replace(/%../g,'i').length;
+        let maxSubstringLength = 50; // speed it up by limiting max length
+        let X, B, O, m, i, c, e, N, M, o, t, j, x, R;
+        let Q = characters;
+        let s = string;
+        X=1;
+        m='';
+        while(true)
+        {
+            for(M=N=e=c=0,i=Q.length;!c&&i--;)!~s.indexOf(Q[i])&&(c=Q[i]);
+            if(!c) break;
+            if(O)
+            {
+                o={};
+                for(x in O)
+                    for(j=s.indexOf(x),o[x]=0;~j;o[x]++)j=s.indexOf(x,j+x.length);
+                O=o;
+            }
+            else for(O=o={},t=1;X&&t<maxSubstringLength;t++)
+                    for(X=i=0;++i<s.length-t;)
+                        if(!o[x=s.substr(j=i,t)])
+                            if(~(j=s.indexOf(x,j+t)))
+                                for(X=t,o[x]=1;~j;o[x]++)j=s.indexOf(x,j+t);
+            for(let x in O) 
+            {
+                j=ByteLength(x);
+                if(j=(R=O[x])*j-j-(R+1)*ByteLength(c))
+                    (j>M||j==M&&R>N)&&(M=j,N=R,e=x);
+                if(j<1)
+                    delete O[x]
+            }
+            o={};
+            for(let x in O)
+                o[x.split(e).join(c)]=1;
+            O=o;
+            if(!e) break;
+            s=s.split(e).join(c)+c+e
+            m=c+m;
+        }
+
+        return {a:s, b:m};
+    }
+
     // remove \u0001 if it is found in the string so it can be used as a delimiter
-    let s = string.replace(/\u0001/g,'');
+    string = string.replace(/\u0001/g,'');
     
     // swap out common json characters
-    s = JSONCrushSwap(s);
+    string = JSONCrushSwap(string);
     
     // create a string of characters that will not be escaped by encodeURIComponent
-    let Q=[];
-    let safeCharacters = `-_.!~*'()`;
+    let characters=[];
+    const unescapedCharacters = `-_.!~*'()`;
     for (let i=127;--i;)
-    (
-        (i>=48&&i<=57) || // 0-9
-        (i>=65&&i<=90) || // A-Z
-        (i>=97&&i<=122)|| // a-z
-        safeCharacters.includes(String.fromCharCode(i))
-    )
-    && Q.push(String.fromCharCode(i));
-    
-    // JSCrush Algorithm (remove repeated substrings)
-    let ByteLength=string=>encodeURI(string).replace(/%../g,'i').length;
-    let X, B, O, m, i, c, e, N, M, o, t, j, x, R;
-    X=B=s.length/2;
-    m='';
-    while(true)
     {
-        for(M=N=e=c=0,i=Q.length;!c&&--i;)!~s.indexOf(Q[i])&&(c=Q[i]);
-        if(!c) break;
-        if(O)
-        {
-            o={};
-            for(x in O)
-                for(j=s.indexOf(x),o[x]=0;~j;o[x]++)j=s.indexOf(x,j+x.length);
-            O=o;
-        }
-        else for(O=o={},t=1;X;t++)
-                for(X=i=0;++i<s.length-t;)
-                    if(!o[x=s.substr(j=i,t)])
-                        if(~(j=s.indexOf(x,j+t)))
-                            for(X=t,o[x]=1;~j;o[x]++)j=s.indexOf(x,j+t);
-        for(let x in O) 
-        {
-            j=ByteLength(x);
-            if(j=(R=O[x])*j-j-(R+1)*ByteLength(c))
-                (j>M||j==M&&R>N)&&(M=j,N=R,e=x);
-            if(j<1)
-                delete O[x]
-        }
-        o={};
-        for(let x in O)
-            o[x.split(e).join(c)]=1;
-        O=o;
-        if(!e) break;
-        s=s.split(e).join(c)+c+e
-        m=c+m;
+        if (
+            (i>=48&&i<=57) || // 0-9
+            (i>=65&&i<=90) || // A-Z
+            (i>=97&&i<=122)|| // a-z
+            unescapedCharacters.includes(String.fromCharCode(i))
+        )
+            characters.push(String.fromCharCode(i));
     }
     
+    // check if every character is used
+    let allUsed = true;
+    for(let i in characters)
+    {
+        let c = characters[i];
+        if (!string.includes(c))
+        {
+            allUsed = false;
+            break;
+        }
+    }
+    
+    if (allUsed)
+    {
+        // use extended set if all the unescaped ones are used
+        for (let i=2;i<255;++i)
+        {
+            let c = String.fromCharCode(i);
+            if (c!='\\' && !characters.includes(c))
+                characters.unshift(c);
+        }
+    }
+    
+    // crush with JS crush
+    let crushed = JSCrush(string, characters);
+    
     // use \u0001 as a delimiter between JSCrush parts 
-    s = s + '\u0001' + m;
+    let crushedString = crushed.a + '\u0001' + crushed.b;
     
     // encode URI
-    return encodeURIComponent(s);
+    return encodeURIComponent(crushedString);
 }
 
 function JSONUncrush(string)
@@ -93,7 +129,7 @@ function JSONUncrush(string)
 function JSONCrushSwap(string, forward=true)
 {
     // swap out characters for lesser used ones that wont get escaped
-    let swapGroups = 
+    const swapGroups = 
     [
         ['"', "'"],
         ["':", "!"],
